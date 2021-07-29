58cee61ef685d39db4231551ce74437220498db4
angujo <angujomondi@gmail.com>
2021-07-29 08:21:46 +0300

    imager resize bug

--- src/components/Imager/Imager.vue
+++ src/components/Imager/Imager.vue
@@ -19,7 +19,7 @@ export default {
     large: Boolean,
     medium: Boolean,
     extraLarge: Boolean,
-    iWidth: {type: Number, default: null},
+    iWidth: {type: [String, Number], default: null},
   },
   computed: {
     cirClass() { return this.circle ? 'rounded-circle' : null; },
@@ -27,7 +27,7 @@ export default {
     rctClass() { return this.rectangle ? 'ratio ratio-4x3' : null;},
     lngClass() { return this.longRectangle ? 'ratio ratio-16x9' : null;},
     banClass() { return this.banner ? 'ratio ratio-21x9' : null;},
-    width() { return this.extraLarge ? 20 : (this.large ? 12 : (this.medium ? 7 : (this.small ? 3 : (this.extraSmall ? 1.5 : this.iWidth))))},
+    width() { return this.extraLarge ? 20 : (this.large ? 12 : (this.medium ? 7 : (this.small ? 3 : (this.extraSmall ? 1.5 : parseFloat(this.iWidth)))))},
     height() {
       if (this.circle || this.square) return 0 + this.width;
       if (this.rectangle) return (3 / 4) * this.width;
